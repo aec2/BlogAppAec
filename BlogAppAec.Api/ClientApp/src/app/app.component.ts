ec80272746d2b0f1e5f9e7f0cc96860bef354b45
AenesC <aenesc1@gmail.com>
2024-09-09 23:40:57 +0300

    UI changed with primeng template

--- BlogAppAec.Api/ClientApp/src/app/app.component.ts
+++ BlogAppAec.Api/ClientApp/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'BlogAppAecClient';
+export class AppComponent implements OnInit {
+    constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit(): void {
+        this.primengConfig.ripple = true;
+    }
 }
